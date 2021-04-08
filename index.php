d58ed9ea1081a470bc20a5f6aa2a66dc01dd6fce
Rombon07 <yustianasrombon7@gmail.com>
2021-04-08 11:39:19 +0700

    kode pertama

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>kalkulator sederhana</title>
+</head>
+<body>
+	<form action= "proses.php" method="post"> 
+		<input type="text" name="angka1">
+		<input type="text" name="angka2">
+		<input type="submit" name="submit" value="tambahkan">
+	</form>
+</body>
+</html>
---
+++ proses.php
@@ -0,0 +1,11 @@
+<?php
+
+	$angka1 = $_POST['angka1'];
+	$angka2 = $_POST['angka2'];
+
+	$hasil = $angka1 * $angka2;
+?>
+
+	<h1>hasilnya adalah <?php echo $hasil ?></h1>
+
+	<a href="index.php">mari kita coba!!!</a>
